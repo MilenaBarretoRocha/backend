2fc2aa3b3880addd6683bbb8fbffc30c1ba702b3
Milena Barreto Rocha <jcmilenasantana@gmail.com>
2024-01-21 17:18:10 -0300

    Subindo os arquivos

---
+++ api/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.get('/', function(req, res){
+res.send("olá mundo");
+});
+app.listen(8080,function () {
+console.log("servidor rodando na porta 8080");
+});

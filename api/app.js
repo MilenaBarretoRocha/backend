e671a1b5d99e807d8c743e8599866e5013656813
Milena Barreto Rocha <jcmilenasantana@gmail.com>
2024-01-21 17:43:40 -0300

    Subindo os arquivos

--- api/app.js
+++ api/app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+const clientes = [
+    {
+        nome: "Maria Alice",
+        cpf: "123456789",
+        rg: "3216547899",
+        end: "Rua da felicidade, 50"
+    },
+    {
+        nome: "Maria Yasmim",
+        cpf: "123456789",
+        rg: "3216547899",
+        end: "Rua da felicidade, 50"
+    }       
+]
 app.get('/', function(req, res){
-res.send("olá mundo");
+return res.json(clientes)
 });
 app.listen(8080,function () {
 console.log("servidor rodando na porta 8080");
